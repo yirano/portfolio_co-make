efdf18dac208d65057f5e2262ffa727b9149b2a2
yirano <yirano@protonmail.ch>
2020-08-16 13:41:36 -0400

    User can log in

--- routers/users-router.js
+++ routers/users-router.js
@@ -1,15 +1,17 @@
 const router = require('express').Router()
 const Users = require('../models/users-model')
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 router.post('/register', async (req, res, next) => {
 	const { username, password, firstName, lastName, city, state } = req.body
 	try {
-		const user = Users.searchUser(username)
+		const user = await Users.searchUser(username).first()
+		console.log(user)
 		if (user) {
 			res.status(409).json({ message: 'Username already taken' })
 		}
-		const newUser = await Users.add({
+		const newUser = await Users.registerUser({
 			username,
 			password: await bcrypt.hash(password, 12),
 			firstName,
@@ -18,6 +20,31 @@ router.post('/register', async (req, res, next) => {
 			state
 		})
 		res.status(201).json(newUser)
+	} catch (error) {
+		next(error)
+	}
+})
+
+router.post('/login', async (req, res, next) => {
+	try {
+		const { username, password } = req.body
+		const user = await Users.searchUser(username).first()
+
+		if (!user) {
+			res.status(401).json({ message: 'Invalid Credentials' })
+		}
+
+		const passwordValid = await bcrypt.compare(password, user.password)
+		if (!passwordValid) {
+			res.status(401).json({ message: 'Invalid Credentials' })
+		}
+		const payload = {
+			id: user.id,
+			username
+		}
+		const token = jwt.sign(payload, process.env.SECRET)
+		res.cookie('token', token)
+		res.status(200).json({ ...payload, token })
 	} catch (error) {
 		next(error)
 	}
